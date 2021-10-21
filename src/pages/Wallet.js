b60585ef4c76b30fa761bc9be9863830e6371999
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2021-10-21 16:28:35 -0300

    requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,7 +6,41 @@ class Wallet extends React.Component {
     return (
       <div>
         <Header />
-        TrybeWallet
+        <form>
+          <label htmlFor="valor">
+            Valor
+            <input type="number" id="valor" name="valor" />
+          </label>
+          <label htmlFor="descricao">
+            Descrição
+            <input type="text" id="descricao" name="descricao" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda">
+              <option value="moeda1">moeda1</option>
+              <option value="moeda2">moeda2</option>
+            </select>
+          </label>
+          <label htmlFor="metodo-pagamento">
+            Método de pagamento
+            <select id="metodo-pagamento">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="cartao-credito">Cartão de Crédito</option>
+              <option value="cartao-debito">Cartão de Débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
